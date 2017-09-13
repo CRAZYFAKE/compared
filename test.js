0be63f52c003de534bf05d266f0269449c05045b
Crizer <553784520@qq.com>
2017-09-13 19:11:21 +0800

    write unit test

--- test.js
+++ test.js
@@ -1,4 +1,30 @@
-var compared = require('./index')
+require('mocha');
+var assert = require('assert');
+var longest = require('./index');
 
-var arr = [333, 1234, '123333333333',null, undefined, (function (a) { }), NaN, '']
-console.log(compared(arr))
+describe('compared', function() {
+  it('should return null when array is empty', function() {
+    assert.equal(longest([]), undefined);
+  });
+
+  it('should throw an error when value is not an array', function() {
+    assert.throws(function() {
+      longest();
+    })
+  });
+
+  it('should return the longest item in the array', function() {
+    assert.equal(longest(['a', 'abcde', 'abc']), 'abcde');
+    assert.equal(longest(['a', 'bb', 'ccc']), 'ccc');
+    assert.equal(longest(['a', 'bbbb', 'ccc']), 'bbbb');
+    assert.equal(longest(['aaaaa', 'bbbb', 'ccc']), 'aaaaa');
+  });
+
+  it('should skip over sparse elements', function() {
+    assert.equal(longest(['a', 'abcde', null, undefined, 'abc']), 'abcde');
+  });
+
+  it('should skip over elements that do not have a .length property', function() {
+    assert.equal(longest(['a', 'abcde', {}, 'abc']), 'abcde');
+  });
+});
